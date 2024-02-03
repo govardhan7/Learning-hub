f6518fa109ae4134a50dfee4a751345d109a9155
Vinith karthik P <vinp@WKMZTF57530B.global.publicisgroupe.net>
2024-02-03 17:49:57 +0530

    card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from './components/Header/Header';
-import './App.css';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './Theme';;
+import theme from './Theme';
 
 function App() {
   return (
