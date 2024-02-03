75a139b4061911cbd7b55de272558d8c3efb44b3
Rayapaneni Govardhan <rayapaneni.govardhan@publicissapient.com>
2024-02-03 18:34:57 +0530

    added BE api and deployment configuration

--- src/data.js
+++ src/data.js
@@ -1,1 +1,263 @@
-let a = 27;
+{
+    users: [
+      {
+        id: 1,
+        username: "Govardhan",
+        role: "Experience Engineer L2",
+        techStack: [
+          {
+            group: "MEAN Stack",
+            technologies: [
+              {
+                name: "JavaScript",
+                courses: [
+                  "JavaScript 101",
+                  "JavaScript 201",
+                  "JavaScript 301"
+                ]
+              },
+              {
+                name: "MongoDB",
+                courses: [
+                  "MongoDB 101",
+                  "MongoDB 201",
+                  "MongoDB 301"
+                ]
+              },
+              {
+                name: "Express",
+                courses: [
+                  "Express 101",
+                  "Express 201",
+                  "Express 301"
+                ]
+              },
+              {
+                name: "Angular",
+                courses: [
+                  "Angular 101",
+                  "Angular 201",
+                  "Angular 301"
+                ]
+              },
+              {
+                name: "Node.js",
+                courses: [
+                  "Node.js 101",
+                  "Node.js 201",
+                  "Node.js 301"
+                ]
+              }
+            ]
+          },
+          {
+            group: "MERN Stack",
+            technologies: [
+              {
+                name: "JavaScript",
+                courses: [
+                  "JavaScript 101",
+                  "JavaScript 201",
+                  "JavaScript 301"
+                ]
+              },
+              {
+                name: "MongoDB",
+                courses: [
+                  "MongoDB 101",
+                  "MongoDB 201",
+                  "MongoDB 301"
+                ]
+              },
+              {
+                name: "Express",
+                courses: [
+                  "Express 101",
+                  "Express 201",
+                  "Express 301"
+                ]
+              },
+              {
+                name: "React",
+                courses: [
+                  "React 101",
+                  "React 201",
+                  "React 301"
+                ]
+              },
+              {
+                name: "Node.js",
+                courses: [
+                  "Node.js 101",
+                  "Node.js 201",
+                  "Node.js 301"
+                ]
+              }
+            ]
+          },
+          {
+            group: "Full Stack",
+            technologies: [
+              {
+                name: "JavaScript",
+                courses: [
+                  "JavaScript 101",
+                  "JavaScript 201",
+                  "JavaScript 301"
+                ]
+              },
+              {
+                name: "Vue.js",
+                courses: [
+                  "Vue.js 101",
+                  "Vue.js 201",
+                  "Vue.js 301"
+                ]
+              },
+              {
+                name: "React",
+                courses: [
+                  "React 101",
+                  "React 201",
+                  "React 301"
+                ]
+              },
+              {
+                name: "Angular",
+                courses: [
+                  "Angular 101",
+                  "Angular 201",
+                  "Angular 301"
+                ]
+              },
+              {
+                name: "Node.js",
+                courses: [
+                  "Node.js 101",
+                  "Node.js 201",
+                  "Node.js 301"
+                ]
+              },
+              {
+                name: "Express",
+                courses: [
+                  "Express 101",
+                  "Express 201",
+                  "Express 301"
+                ]
+              },
+              {
+                name: "Flask",
+                courses: [
+                  "Flask 101",
+                  "Flask 201",
+                  "Flask 301"
+                ]
+              },
+              {
+                name: "Spring Boot",
+                courses: [
+                  "Spring Boot 101",
+                  "Spring Boot 201",
+                  "Spring Boot 301"
+                ]
+              },
+              {
+                name: ".NET Core",
+                courses: [
+                  ".NET Core 101",
+                  ".NET Core 201",
+                  ".NET Core 301"
+                ]
+              },
+              {
+                name: "MongoDB",
+                courses: [
+                  "MongoDB 101",
+                  "MongoDB 201",
+                  "MongoDB 301"
+                ]
+              },
+              {
+                name: "MySQL",
+                courses: [
+                  "MySQL 101",
+                  "MySQL 201",
+                  "MySQL 301"
+                ]
+              },
+              {
+                name: "AWS",
+                courses: [
+                  "AWS 101",
+                  "AWS 201",
+                  "AWS 301"
+                ]
+              },
+              {
+                name: "Heroku",
+                courses: [
+                  "Heroku 101",
+                  "Heroku 201",
+                  "Heroku 301"
+                ]
+              },
+              {
+                name: "Docker",
+                courses: [
+                  "Docker 101",
+                  "Docker 201",
+                  "Docker 301"
+                ]
+              },
+              {
+                name: "Kubernetes",
+                courses: [
+                  "Kubernetes 101",
+                  "Kubernetes 201",
+                  "Kubernetes 301"
+                ]
+              },
+              {
+                name: "Jenkins",
+                courses: [
+                  "Jenkins 101",
+                  "Jenkins 201",
+                  "Jenkins 301"
+                ]
+              },
+              {
+                name: "GitHub Actions",
+                courses: [
+                  "GitHub Actions 101",
+                  "GitHub Actions 201",
+                  "GitHub Actions 301"
+                ]
+              }
+            ]
+          },
+          {
+            group: "Cloud",
+            technologies: [
+              {
+                name: "AWS",
+                courses: [
+                  "AWS 101",
+                  "AWS 201",
+                  "AWS 301"
+                ]
+              },
+              {
+                name: "Heroku",
+                courses: [
+                  "Heroku 101",
+                  "Heroku 201",
+                  "Heroku 301"
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
