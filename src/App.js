13100573df0e39364a9cc71902cd73627512b1d4
Rayapaneni Govardhan <rayapaneni.govardhan@publicissapient.com>
2024-02-03 15:42:24 +0530

    intial code push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DefaultButton from './atoms/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <DefaultButton></DefaultButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/atoms/Button/Button.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function DefaultButton() {
+  return (
+      <Button variant="text">Text</Button>
+  );
+}
